c7b2138f61a02c61ad52331140ea20be547a0cb3
Chris <c.uzor@yahoo.com>
2020-10-23 01:23:42 +0100

    About page added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageWrapper from './components/PageWrapper';
 import Home from './components/Pages/home';
+import About from './components/Pages/about';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 function App() {
   return (
@@ -8,9 +9,16 @@ function App() {
     <PageWrapper>
      
         <Route 
+        exact={true}
         path="/"
         component={Home}
         />
+        <Route
+        path="/about"
+        component={About}
+        />
+
+        
      
      
     </PageWrapper>
